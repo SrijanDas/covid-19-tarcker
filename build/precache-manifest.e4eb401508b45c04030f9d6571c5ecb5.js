d7ff7b654c1a3f00214967dcdeb3e1a6e88a0a48
SrijanDas <srijandas7.sd@gmail.com>
2020-09-10 11:29:56 +0530

    build

---
+++ build/precache-manifest.e4eb401508b45c04030f9d6571c5ecb5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "677f47ce881e07b156ace78ab1b345cc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "96daa5b283804ac9b2e6",
+    "url": "/static/css/2.a4567992.chunk.css"
+  },
+  {
+    "revision": "8b84972b00423e5f65a4",
+    "url": "/static/css/main.c41b60de.chunk.css"
+  },
+  {
+    "revision": "96daa5b283804ac9b2e6",
+    "url": "/static/js/2.e6c6024a.chunk.js"
+  },
+  {
+    "revision": "ebc70c097554e9e2ef968c42fad2beba",
+    "url": "/static/js/2.e6c6024a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b84972b00423e5f65a4",
+    "url": "/static/js/main.930756f2.chunk.js"
+  },
+  {
+    "revision": "3ff5d6fb19a2b7a4acf2",
+    "url": "/static/js/runtime-main.8b39ddf8.js"
+  }
+]);
